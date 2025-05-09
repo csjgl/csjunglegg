75233fd9e455fe77976717459619457a6c776e07
atheno79 <asosondingistdas@gmail.com>
2025-05-09 17:05:02 +0200

    Trigger redeploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      zIndex: {
+        51: '51',
+      },
+    },
   },
   plugins: [],
   purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
