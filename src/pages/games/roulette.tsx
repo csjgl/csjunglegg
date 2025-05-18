4be4522590cdefe3d7e16dedce5f03282915c8be
atheno79 <asosondingistdas@gmail.com>
2025-05-18 19:15:18 +0200

    Trigger redeploy

--- src/pages/games/roulette.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import RouletteGame from '../../components/RouletteGame';
-
-const RouletteGamePage: React.FC = () => {
-  return <RouletteGame />;
-};
-
-export default RouletteGamePage;
